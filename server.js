78ca7dcf3f04545ccb6e7214a74a96593f51d344
javesh13 <javesh13@gmail.com>
2016-09-18 15:02:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/article-three',function(req,res){
     res.send("Artical three requested and will be served here");
     
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
