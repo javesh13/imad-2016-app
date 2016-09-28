81b681aae820402316eddbaea4e8c7d0c399cbfd
javesh13 <javesh13@gmail.com>
2016-09-28 16:30:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counter valuer
+var buttom=document.getElementById("counter");
+button.onclick = function()
+{
+    counter = counter+l;
+    var span=document.getElemetById("count");
+    sapn.innerHTML=counter.toString();
+};
