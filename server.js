329a2e9cae837be3fbfa950fc9264e9978c0fdd5
javesh13 <javesh13@gmail.com>
2016-09-28 16:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/article-three',function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('main.js',function(re,res){
+app.get('/ui/main.js',function(re,res){
     res.sendFile(path.join(__dirname,'ui', 'main.js'));
 });
 app.get('/ui/madi.png', function (req, res) {
