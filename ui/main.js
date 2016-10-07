847b3bd27ec66f63b854cd05d830235039181c49
javesh13 <javesh13@gmail.com>
2016-10-07 17:39:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,21 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick = function()
 {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   
+    var request =  new XMLHttpRequest();
+    
+    
+     if(request.readystate===XMLHttpRequest.DONE){
+         
+     if(request.status===200){
+    request.onreadystatechange=function(){
+        var counter =request.responseText();
+        var span = document.getElementById('count');
+        span.innerHtml=counter.toString();
+        }
+     }
+        
+ }
+ request.open('GET','http://javesh13.imad.hasura-app.io/counter',true);
+ request.send(null);
 };
